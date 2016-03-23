96adda0be9e08a393671b373f9c5bb3ef9b1b1bc
Delisa Mason <iskanamagus@gmail.com>
2016-03-22 20:26:09 -0700

    Split array and string logic from hashes

--- lib/bugsnag/helpers.rb
+++ lib/bugsnag/helpers.rb
@@ -3,6 +3,7 @@ require 'uri'
 module Bugsnag
   module Helpers
     MAX_STRING_LENGTH = 4096
+    TRUNCATION = '[TRUNCATED]'
 
     def self.reduce_hash_size(hash)
       return {} unless hash.is_a?(Hash)
@@ -10,14 +11,36 @@ module Bugsnag
         if v.is_a?(Hash)
           h[k] = reduce_hash_size(v)
         elsif v.is_a?(Array) || v.is_a?(Set)
-          h[k] = v.map {|el| reduce_hash_size(el) }
+          h[k] = reduce_array_size(v)
         else
-          val = v.to_s
-          val = val.slice(0, MAX_STRING_LENGTH) + "[TRUNCATED]" if val.length > MAX_STRING_LENGTH
-          h[k] = val
+          h[k] = reduce_string_size(v)
         end
 
         h
+      end
+    end
+
+    def self.reduce_string_size(text)
+      return '' unless text.respond_to?(:to_s)
+      val = text.to_s
+      if val.length > MAX_STRING_LENGTH
+        val.slice(0, MAX_STRING_LENGTH - TRUNCATION.length) + TRUNCATION
+      end
+      val
+    end
+
+    def self.reduce_array_size(ary)
+      return [] unless ary.is_a?(Array) or ary.is_a?(Set)
+      ary.map do |item|
+        reduce_hash_size(item)
+        case item
+        when Array, Set
+          reduce_array_size(item)
+        when Hash
+          reduce_hash_size(item)
+        else
+          reduce_string_size(item)
+        end
       end
     end
 
